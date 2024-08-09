8d417899c2971a7b53f8497ee2624e70017227b0
Anna Panferova <anyapanf@gmail.com>
2024-08-09 14:48:33 +0600

    Bugs fixed

--- src/App.js
+++ src/App.js
@@ -9,14 +9,16 @@ function App() {
   return (
     <div className={AppCss.container}>
       <NestedCategoriesSelector data={data} setSelectedIds={setSelectedIds} selectedIds={selectedIds} />
-      <div className={AppCss.selected}>
+      {/* <div className={AppCss.selected}>
         <h3>Все выбранные категории</h3>
         <ul className={AppCss.selectedList}>
-          {selectedIds.map(id => (
-            <li className="topItems">{data.find(obj => obj.id === +id).name}</li>
-          ))}
+          {selectedIds.map(id => {
+            const category = data.find(obj => obj.id === +id);
+            if (!category) return null;
+            return <li className="topItems" key={id}>{category.name}</li>;
+          })}
         </ul>
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/components/MenuSubItem.js
+++ src/components/MenuSubItem.js
@@ -3,37 +3,48 @@ import { useState } from "react"
 import MenuSubitemCss from './MenuSubItem.module.css'
 
 export const MenuSubItem = ({ ids, id2category, selectedIds, toggleId }) => {
-    const [unfold, setUnfold] = useState([])
+    const [unfold, setUnfold] = useState([]);
 
     const handleClick = (productId) => {
-        setUnfold(prev => prev.includes(productId) ? prev.filter((id) => id !== productId) : [...prev, productId])
-    }
+        setUnfold(prev => prev.includes(productId) ? prev.filter((id) => id !== productId) : [...prev, productId]);
+    };
 
     return (
         <ul className={MenuSubitemCss.menu}>
             {ids.map((id) => {
-                const { children, name } = id2category[id]
+                const { children, name } = id2category[id];
 
                 return (
-                    <li className={MenuSubitemCss.menuItem}>
-                        {children.length > 0 && (
-                            <div className={MenuSubitemCss.line}>
-                                <button className={MenuSubitemCss.btn}
-                                    onClick={() => handleClick(id)}>
-                                    <img src={arrow}
-                                        className={unfold.includes(id)
-                                            ? `${MenuSubitemCss.menuArrowTurned} ${MenuSubitemCss.selected}`
-                                            : `${MenuSubitemCss.menuArrow}`} />
-                                </button>
-                                <input type="checkbox" id={id} checked={selectedIds.includes(id)} onChange={() => toggleId(id)}></input>
-                                <label for={id} className={MenuSubitemCss.menuText}>{name}</label>
-                            </div>)}
+                    <li className={MenuSubitemCss.menuItem} key={id}>
+                        {children.length > 0
+                            ? (
+                                <div className={MenuSubitemCss.line}>
+                                    <button className={MenuSubitemCss.btn} onClick={() => handleClick(id)}>
+                                        <img src={arrow}
+                                            className={unfold.includes(id)
+                                                ? `${MenuSubitemCss.menuArrowTurned}`
+                                                : `${MenuSubitemCss.menuArrow}`} />
+                                    </button>
+                                    <input type="checkbox" id={id} checked={selectedIds.includes(id)} onChange={() => toggleId(id)} />
+                                    <label htmlFor={id} className={MenuSubitemCss.menuText}>{name}</label>
+                                </div>
+                            )
+                            : (
+                                <div className={MenuSubitemCss.line}>
+                                    <button className={MenuSubitemCss.btn} onClick={() => handleClick(id)}>
+                                        <img src={arrow}
+                                            className={MenuSubitemCss.hidden} />
+                                    </button>
+                                    <input type="checkbox" id={id} checked={selectedIds.includes(id)} onChange={() => toggleId(id)} />
+                                    <label htmlFor={id} className={MenuSubitemCss.menuText}>{name}</label>
+                                </div>
+                            )}
                         {children.length > 0 && unfold.includes(id) && (
                             <MenuSubItem ids={children} id2category={id2category} selectedIds={selectedIds} toggleId={toggleId} />
                         )}
                     </li>
-                )
+                );
             })}
         </ul>
-    )
+    );
 };
